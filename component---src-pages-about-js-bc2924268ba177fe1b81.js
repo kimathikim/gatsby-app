ee4fd1b361fbdc3363c1ca468ad89cbd7b5866fc
kimathikim <briankimathi94@gmail.com>
2023-10-31 01:44:50 +0300

    adding About page to site

---
+++ component---src-pages-about-js-bc2924268ba177fe1b81.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[682],{5465:function(e,t,a){a.r(t);var l=a(7294),n=a(1883);t.default=e=>{let{data:t}=e;return l.createElement(l.Fragment,null,l.createElement("h2",null,t.site.siteMetadata.title),l.createElement("div",null,t.site.siteMetadata.description),l.createElement(n.Link,{to:"/"},"Back to Home"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-bc2924268ba177fe1b81.js.map
